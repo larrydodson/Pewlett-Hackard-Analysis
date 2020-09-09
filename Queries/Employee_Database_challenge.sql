dd4ed7453a547a1b5534aec4bdd9c9e7eae7dda8
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-09 17:32:51 -0500

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,39 @@
+-- Module 7 Challenge
+--   Deliverable-1 The Number of Retiring Employees 
+
+SELECT * FROM employees;
+SELECT * FROM titles;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
