4d00cbe58c9a17f285063ddb3995de5814aea1a0
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-09 23:27:38 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -19,14 +19,26 @@ ORDER BY emp_no, to_date DESC
 --ORDER BY emp_no, from_date
 ;
 
+-- Creating unique_titles table
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name, last_name,
 	title
-INTO unique_titles
+-- INTO unique_titles
 FROM retirement_titles as rt 
 ORDER BY emp_no, to_date DESC
 ;
+
+-- Creating retiring_titles table
+SELECT COUNT(ut.title), ut.title
+-- INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY ut.count DESC
+;
+
+
+
 
 
 
