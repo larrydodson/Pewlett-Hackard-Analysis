0cf24d032d916be0f5e1f3c895df381bf27b9008
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-09 21:47:21 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -3,6 +3,19 @@
 
 SELECT * FROM employees;
 SELECT * FROM titles;
+SELECT * FROM dept_emp;
+
+-- Creating retirement_titles table
+SELECT e.emp_no,
+	e.first_name, e.last_name,
+	tt.title, tt.from_date, tt.to_date
+-- INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as tt 
+	ON e.emp_no = tt.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no
+;
 
 
 
@@ -20,13 +33,15 @@ SELECT * FROM titles;
 
 
 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
 
-
-
-
-
-
-
+INTO nameyourtable
+FROM _______
+ORDER BY _____, _____ DESC;
 
 
 
