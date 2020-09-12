bc61a383842d95b2e4640a71acaebc6a63ac6103
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-12 15:12:08 -0500

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -87,7 +87,7 @@ CREATE TABLE titles (
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
-SELECT * FROM departments;
+SELECT * FROM titles;
 
 -- Sect 7.3.1 Query Dates
 SELECT first_name, last_name
@@ -123,4 +123,164 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info;
+
+-- Sect 7.3.2 Joining 
+-- Drop the current retirement_info table
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Sect 7.3.3 Joining 
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Renaming with aliases exercise
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Use Left Join for retirement_info and dept_emp tables
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Sect 7.3.4 Use Count, Group By, and Order By
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- same code above including creating a new table 
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retirement_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Sect 7.3.5 additional lists
+
+SELECT * FROM retirement_dept;
+SELECT * FROM retirement_info;
+SELECT * FROM dept_info;
+
+-- Creating emp_info
+SELECT emp_no,
+    first_name,
+	last_name,
+    gender
+-- INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
+
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+-- INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+-- INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+
+SELECT * FROM emp_info;
+SELECT * FROM current_emp;
+SELECT * FROM retirement_dept;
+SELECT * FROM retirement_info;
+SELECT * FROM dept_info;
+SELECT * FROM manager_info;
+
+-- Sect 7.3.6 
+
+SELECT * FROM dept_info
+ORDER BY dept_name DESC;
+
+SELECT * FROM dept_info
+ORDER BY dept_name;
+
+-- Create emp-sales
+
+-- first of two methods
+SELECT * 
+--INTO Sales_Dev_emp
+FROM dept_info
+WHERE dept_name = 'Sales'
+OR dept_name ='Development'
+ORDER BY dept_name DESC;
+
+-- second method using IN with WHERE 
+SELECT * 
+-- INTO sales_dev_emp
+FROM dept_info
+WHERE dept_name IN ('Sales', 'Development')
+ORDER BY dept_name;
+
+
+
+
+
+
+
 
