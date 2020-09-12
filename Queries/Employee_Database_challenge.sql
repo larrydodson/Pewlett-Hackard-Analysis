ff1a87188b863e3d1c20701b08174a680859adb9
Larry Dodson <67447445+larrydodson@users.noreply.github.com>
2020-09-12 18:19:44 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,6 +6,7 @@
 -- SELECT * FROM titles;
 -- SELECT * FROM dept_emp;
 -- SELECT * FROM retirement_titles;
+-- SELECT * FROM departments;
 
 -- Creating retirement_titles table
 SELECT e.emp_no,
@@ -37,6 +38,31 @@ GROUP BY title
 ORDER BY ut.count DESC
 ;
 
+-- Creating a retiring count by department table 
+-- for comparison and for the Challenge Report submission
+-- SELECT * FROM unique_titles;
+
+SELECT ut.emp_no, de.dept_no
+--, di.dept_name
+--INTO rdn
+FROM unique_titles as ut
+INNER JOIN dept_emp AS de
+	ON ut.emp_no = de.emp_no
+;
+
+SELECT COUNT(rdn.dept_no), rdn.dept_no
+--INTO rdn_name
+FROM rdn
+GROUP BY rdn.dept_no
+ORDER BY rdn.count DESC
+;
+SELECT rdnn.dept_no, rdnn.count, d.dept_name
+--INTO retire_dept
+FROM rdn_name AS rdnn
+INNER JOIN departments AS d
+	ON rdnn.dept_no = d.dept_no
+;
+
 -- Deliverable 2: The Employees Eligible for the Mentorship Program
 --   Creating mentorship_cand table, as interim candidate table
 SELECT e.emp_no,
@@ -60,7 +86,7 @@ ORDER BY e.emp_no, tt.from_date DESC
 SELECT DISTINCT ON (emp_no) emp_no,
 	first_name, last_name, birth_date,
 	from_date, to_date, title
-INTO mentorship_eligibilty
+--INTO mentorship_eligibilty
 FROM mentorship_cand AS mc 
 ORDER BY emp_no
 ;
